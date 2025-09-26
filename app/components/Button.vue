d45a8663e2aaf4834f8e4a440944564b667a07d1
Abdurrahman Seyidoğlu <abdurrahmanseyidoglu@gmail.com>
2025-09-26 17:49:02 +0300

    feat: Add base libraries and setup tailwind

---
+++ app/components/Button.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { ButtonVariants } from '~/types/ButtonVariants';
+
+defineProps({
+    label: {
+        type: String,
+
+    },
+    variant: {
+        type: String as PropType<ButtonVariants>
+    }
+})
+</script>
+<template>
+    <button class="">{{label}}</button>
+</template>
