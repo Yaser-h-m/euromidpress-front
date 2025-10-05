b63a6c923935654f79365770cee07cc5b251736b
Abdurrahman Seyidoğlu <abdurrahmanseyidoglu@gmail.com>
2025-10-05 21:03:31 +0300

    feat:
    - Add PartnerLogo component with a demo page
    
    - Fix breakpoints to match tailwinds one
    
    - move demo pages to demo folder
    
    - remove unused data-nuxt-img

---
+++ app/pages/demos/partners-logos-demo.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const logos = [
+    { src: '/img/partners-logos/clarivate.png', alt: 'Clarivate' },
+    { src: '/img/partners-logos/crossref.png', alt: 'Crossref' },
+    { src: '/img/partners-logos/doaj.png', alt: 'DOAJ' },
+    { src: '/img/partners-logos/scopus.png', alt: 'Scopus' },
+    { src: '/img/partners-logos/society-for-scholarly.png', alt: 'Society for Scholarly' }
+]
+</script>
+
+<template>
+    <div class="flex flex-wrap gap-6 p-8 justify-center items-center">
+        <CommonPartnerLogo
+            v-for="logo in logos"
+            :key="logo.src"
+            :logo-src="logo.src"
+            :logo-alt="logo.alt"
+        />
+    </div>
+</template>
