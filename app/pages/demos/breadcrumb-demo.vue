ab0ed6a1517d16c441b16e0ff9683647bb92a84b
Khaled Nadam <khalednadam@gmail.com>
2025-10-21 19:57:41 +0300

    feat: Add breadcrumb demo page with static breadcrumb items

---
+++ app/pages/demos/breadcrumb-demo.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { BreadcrumbItem } from '~/types/BreadcrumbItem'
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        label: 'Home',
+        link: '/'
+    },
+    {
+        label: 'Articles',
+        link: '/articles'
+    },
+    {
+        label: 'Article 1',
+        link: '/articles/article-1'
+    }
+]
+</script>
+<template>
+    <div class="flex flex-col gap-2 p-8">
+        <CommonBreadcrumb :breadcrumb-items="breadcrumbs" />
+    </div>
+</template>
