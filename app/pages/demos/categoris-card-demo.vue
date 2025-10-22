e72a89249794a3183a45194d18ec11c72f128a87
Abdurrahman Seyidoğlu <abdurrahmanseyidoglu@gmail.com>
2025-10-22 20:21:31 +0300

    feat: Add CategoryCard component and a demo page for it

---
+++ app/pages/demos/categoris-card-demo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-wrap gap-14 p-8 justify-center items-start bg-white-0 shadow-lg rounded-2xl m-3">
+        <CommonCategoryCard
+            v-for="i in 6"
+            :key="i"
+            title="Computer Science & Artificial Intelligence"
+            link="https://www.google.com/"
+            proceedings-title="Advances in Computer Science Research (ACSR)"
+            proceedings-link="#"
+            books-title="AI Books Collection"
+            books-link="#"
+            :fluid="true"
+            class="w-[400px]"
+        />
+    </div>
+</template>
