779bb752bcfe730693990b4315fd26fe9809aade
Khaled Nadam <khalednadam@gmail.com>
2025-10-29 07:22:59 +0300

    feat: Implement ContactForm component with reactive form handling and demo page

---
+++ app/components/Common/ContactForm.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import TextField from './TextField.vue'
+
+
+const form = reactive({
+    name: '',
+    email: '',
+    message: ''
+})
+const handleSubmit = () => {
+    console.log(form)
+}
+</script>
+<template>
+    <div>
+        <p class="font-semibold text-xl md:text-2xl text-primary-500 text-center">
+            Have a question? Send us a message using the form below
+        </p>
+        <form class="mt-8 flex flex-col gap-4" @submit.prevent="handleSubmit">
+            <TextField id="name" v-model="form.name" label="Name" />
+            <TextField id="email" v-model="form.email" label="Email" />
+            <FloatLabel variant="on">
+                <Textarea
+                    id="message"
+                    v-model="form.message"
+                    class="w-full border !border-white-300 ring-0 !focus-visible:ring-0 rounded-lg !focus-visible:ring-primary-500 !bg-white-100 focus:!border-white-300 !text-primary-500"
+                    rows="5"
+                    cols="30"
+                    style="resize: none"
+                />
+                <label for="message" class="!text-primary-500 !bg-white-100">Your Message</label>
+            </FloatLabel>
+            <CommonButton label="Submit" variant="secondary-btn" />
+        </form>
+    </div>
+</template>
+
+<style scoped>
+input:focus {
+    outline: none;
+}
+input {
+  padding: 12px;
+}
+</style>
---
+++ app/pages/demos/contact-form-demo.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div class="flex ">
+        <div class="w-1/2" />
+        <CommonContactForm />
+    </div>
+</template>
